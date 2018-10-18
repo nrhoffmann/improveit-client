dbfde1a0043805ad6c513e515375c58e502ea7b4
Nathan Hoffmann <nathanrosshoffmann@gmail.com>
2018-10-18 09:29:18 -0400

    adds google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
           <meta charSet="UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,600"/>
           {this.props.styleTags}
         </Head>
         <body className="container">
